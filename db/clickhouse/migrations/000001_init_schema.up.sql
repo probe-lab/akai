6ef5ac09465d8b80f6a9e56ff742bb32aff8787f
Dennis Trautwein <git@dtrautwein.eu>
2024-12-05 11:55:56 +0100

    update: database schema

--- db/clickhouse/migrations/000001_init_schema.up.sql
+++ db/clickhouse/migrations/000001_init_schema.up.sql
@@ -1,50 +1,57 @@
-CREATE TABLE IF NOT EXISTS networks (
-    network_id UInt16,
-    protocol String,
+CREATE TABLE IF NOT EXISTS networks
+(
+    network_id   UInt16,
+    protocol     String,
     network_name String
 )
-ENGINE = MergeTree()
-PRIMARY KEY (network_id);
+ENGINE = ReplicatedMergeTree()
+    PRIMARY KEY (network_id);
 
 
-CREATE TABLE IF NOT EXISTS blobs (
-    network_id UInt16,
-    timestamp DateTime,
-    hash String,
-    key String,
-    blob_number UInt64,
-    rows UInt32,
-    columns UInt32,
+CREATE TABLE IF NOT EXISTS blobs
+(
+    network_id   UInt16,
+    timestamp    DateTime,
+    hash         String,
+    key          String,
+    blob_number  UInt64,
+    rows         UInt32,
+    columns      UInt32,
     sample_until DateTime
 )
-ENGINE = MergeTree()
-PRIMARY KEY (hash, blob_number);
+ENGINE = ReplicatedMergeTree()
+    PRIMARY KEY (hash, blob_number)
+TTL toDateTime(timestamp) + INTERVAL 180 DAY;
 
 
-CREATE TABLE IF NOT EXISTS segments (
-    timestamp DateTime,
-    blob_number UInt64,
-    key String,
-    row UInt32,
-    column UInt32,
+CREATE TABLE IF NOT EXISTS segments
+(
+    timestamp    DateTime,
+    blob_number  UInt64,
+    key          String,
+    row          UInt32,
+    column       UInt32,
     sample_until DateTime
 )
-ENGINE = MergeTree()
-PRIMARY KEY (blob_number, key);
+ENGINE = ReplicatedMergeTree()
+    PRIMARY KEY (blob_number, key)
+TTL toDateTime(timestamp) + INTERVAL 180 DAY;
 
 
-CREATE TABLE IF NOT EXISTS visits (
-    visit_round UInt64,
-    timestamp DateTime,
-    key String,
-    blob_number UInt64,
-    row UInt32,
-    column UInt32,
-    duration_ms Int64,
+CREATE TABLE IF NOT EXISTS visits
+(
+    visit_round    UInt64,
+    timestamp      DateTime,
+    key            String,
+    blob_number    UInt64,
+    row            UInt32,
+    column         UInt32,
+    duration_ms    Int64,
     is_retrievable Bool,
-    providers UInt32,
-    bytes UInt32,
-    error String
+    providers      UInt32,
+    bytes          UInt32,
+    error          String
 )
-ENGINE = MergeTree()
-PRIMARY KEY (key);
+ENGINE = ReplicatedMergeTree()
+    PRIMARY KEY (visit_round)
+TTL toDateTime(timestamp) + INTERVAL 180 DAY;
